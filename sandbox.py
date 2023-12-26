c8951f295dad1244f425c2f5832c22789f8b2737
Alexene Tomate <alekzene.cs@gmail.com>
2023-12-26 20:59:18 +0800

    Test Jupyter Notebook

---
+++ sandbox.py
@@ -0,0 +1,19 @@
+# test
+i = 2
+if (i == 2):
+  print(2)
+
+# random thought: can i use !<= to say "not less than or equal to"?
+# i know it's inconvenient and we can just use >
+# i just got curious
+  
+# if (i !<= 1):
+#   print(1)
+
+# ok... we can't
+
+""" multi-line comment """
+
+is_true = True # note: true is not defined in Python; use True
+
+# Why do we use Jupyter Notebook? What is the value it offers to the data science and computer science community?
